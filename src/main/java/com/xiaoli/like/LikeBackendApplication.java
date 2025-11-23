0f00e8c7234b0dbaf56c92492740672a00da5209
codeScanf <lxiaobin10@gmail.com>
2025-11-23 18:11:56 +0800

    v1.1 项目初始化1

---
+++ src/main/java/com/xiaoli/like/LikeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoli.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoli/like/LikeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoli.like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
